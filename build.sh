a532e2622bd4e5a470fdaaaca0f356b23be1bb33
Benjamin Pollack <benjamin@bitquabit.com>
2013-05-30 23:30:27 +0400

    only set PKG_CONFIG_PATH on OS X

--- build.sh
+++ build.sh
@@ -9,7 +9,10 @@ cmake ..
 cmake --build .
 
 cd $(hg root)
-export PKG_CONFIG_PATH=/usr/local/Cellar/libffi/3.0.13/lib/pkgconfig
+if [$(uname) = "Darwin"]
+then
+    export PKG_CONFIG_PATH=/usr/local/Cellar/libffi/3.0.13/lib/pkgconfig
+fi
 test -d venv && rm -r venv
 virtualenv venv
 venv/bin/pip install cffi

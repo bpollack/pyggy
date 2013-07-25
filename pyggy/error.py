ce67209299a9d8928efc3982d74c1770a5a7ed58
Benjamin Pollack <benjamin@bitquabit.com>
2013-07-25 13:44:31 -0400

    error: allow GitException to get extra data

--- pyggy/error.py
+++ pyggy/error.py
@@ -2,7 +2,8 @@ from .core import lib, ffi
 
 
 class GitException(Exception):
-    def __init__(self):
+    def __init__(self, extra=None):
         error = lib.giterr_last()
         message = ffi.string(error.message) if error != ffi.NULL else 'unknown error'
         super(GitException, self).__init__(message)
+        self.extra = extra
